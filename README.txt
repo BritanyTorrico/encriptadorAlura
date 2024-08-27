ab0ae6ebf4080bb1d871295378d6956a192affe3
BritanyTorrico <torricobritany2@gmail.com>
2024-08-26 20:55:20 -0400

    proyecto encriptador challenge alura

---
+++ README.txt
@@ -0,0 +1,45 @@
+# Codificador y Decodificador de Texto
+
+Este proyecto es una aplicación web sencilla que permite codificar y decodificar texto utilizando un método de sustitución de caracteres.
+
+## Funcionalidades
+
+- **Codificación:** Transforma el texto ingresado en un formato codificado usando una serie de reglas de sustitución.
+- **Decodificación:** Restaura el texto codificado a su formato original.
+- **Copiar al portapapeles:** Facilita la copia del texto codificado o decodificado al portapapeles con un solo clic en un boton.
+- **Interfaz amigable:** La aplicación cuenta con una interfaz de usuario intuitiva y es responsive.
+
+## Reglas de Codificación
+
+El texto se codifica siguiendo las siguientes reglas de sustitución:
+
+- `e` se convierte en `enter`
+- `i` se convierte en `imes`
+- `a` se convierte en `ai`
+- `o` se convierte en `ober`
+- `u` se convierte en `ufat`
+
+De manera similar, la decodificación sigue estas mismas reglas para revertir el texto a su forma original.
+
+## Como usar?
+
+1. Escribe el texto que deseas codificar o decodificar en el área de texto de la izquierda.
+2. Haz clic en el botón "Codificar" para transformar el texto, o en "Decodificar" para revertirlo a su estado original.
+3. El resultado aparecerá en el área de texto de la derecha.
+4. Puedes copiar el texto generado al portapapeles usando el botón "Copiar".
+
+## Requisitos previos
+
+Para ejecutar este proyecto localmente, solo necesitas un navegador web actualizado. No se requiere ninguna instalación adicional.
+
+## Estructura del Proyecto
+
+- `index.html`: El archivo principal que contiene la estructura HTML de la aplicación.
+- `style.css`: Archivo de estilos CSS que define la apariencia de la interfaz de usuario.
+- `script.js`: Archivo JavaScript que contiene la lógica para codificar y decodificar el texto.
+- `assets/`: Carpeta que almacena las imágenes utilizadas en la interfaz (logotipo, iconos, etc.).
+## Hecho por:
+
+###Britany Torrico Rosas
+
+### Linkedin: https://www.linkedin.com/in/britany-torrico-rosas
---
+++ script.js
@@ -0,0 +1,99 @@
+const textArea= document.querySelector(".text-area");
+const mensaje= document.querySelector(".mensaje");
+const mensajeAdicional = document.querySelector(".mensaje-adicional");
+const copiarBtn = document.querySelector('.btn-copiar');
+const contenedorDerecha = document.querySelector('.output-section');
+const contenedorIzquierda = document.querySelector('.contenedor-izquierda');
+const mensajeValidacion = document.getElementById("mensaje-validacion");
+function btnEncriptar() {
+    if (validarTexto(textArea.value)) {
+        const textoEncriptado = encriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        textArea.value = "";
+        verificarTextArea();
+        mensajeValidacion.style.display = "none"; 
+    }else{
+        mensajeValidacion.style.display = "flex";
+    }
+}
+function btnDesencriptar() {
+    if (validarTexto(textArea.value)) {
+        const textoEncriptado = desencriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        textArea.value = "";
+        verificarTextArea();
+        mensajeValidacion.style.display = "none"; 
+    } else {
+        mensajeValidacion.style.display = "flex"; 
+    }
+}
+
+function validarTexto(texto) {
+    const regex = /^[a-z\s]*$/;
+    if (!regex.test(texto)) {
+        return false;
+    }
+    return true;
+}
+
+function encriptar(valor){
+    let matriz = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    valor = valor.toLowerCase();
+
+    for (let i = 0; i < matriz.length; i++) {
+        if (valor.includes(matriz[i][0])) {
+            valor = valor.replaceAll(matriz[i][0], matriz[i][1]);
+        }
+    }
+    return valor;
+}
+
+function desencriptar(valor){
+    let matriz = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    valor = valor.toLowerCase();
+    for (let i = 0; i < matriz.length; i++) {
+        if (valor.includes(matriz[i][1])) {
+            valor = valor.replaceAll(matriz[i][1], matriz[i][0]);
+        }
+    }
+    
+    return valor;
+}
+function verificarTextArea() {
+    if (mensaje.value.trim() === "") {
+        mensajeAdicional.style.display = "block";
+        mensaje.style.backgroundImage = 'url("/assets/Muñeco.png")'; 
+        copiarBtn.style.display = "none";
+
+    } else {
+        mensajeAdicional.style.display = "none";
+        mensaje.style.backgroundImage="none";
+        copiarBtn.style.display = "block"; 
+        mensaje.style.display="block";
+        mensaje.style.maxHeight="100%";
+    }
+}
+function copiarAlPortapapeles() {
+    mensaje.select();
+    mensaje.setSelectionRange(0, 99999); 
+    document.execCommand("copy");
+    alert("Texto copiado al portapapeles");
+}
+
+// Verifica el text area inicialmente y cada vez que cambia su contenido
+textArea.addEventListener("input", verificarTextArea);
+
+// Ejecuta la verificación inicial al cargar la página
+verificarTextArea();
