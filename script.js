9d33c0842a98eb2c6cbcee8493a13308f95a3048
BritanyTorrico <torricobritany2@gmail.com>
2024-08-26 21:43:25 -0400

    fix ruta imagenes

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function desencriptar(valor){
 function verificarTextArea() {
     if (mensaje.value.trim() === "") {
         mensajeAdicional.style.display = "block";
-        mensaje.style.backgroundImage = 'url("./assets/Muñeco.png")'; 
+        mensaje.style.backgroundImage = 'url("./assets/Muneco.png")'; 
         copiarBtn.style.display = "none";
 
     } else {
